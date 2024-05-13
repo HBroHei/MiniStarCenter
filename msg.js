7dc6876844b6bbaa4aa2d854819f665a01f1bfa0
HBroHei <hammerbrohei@gmail.com>
2024-05-13 23:18:16 +0800

    Updated color themes

---
+++ msg.js
@@ -0,0 +1,6 @@
+const msgList = [
+            'Player Conveyor is a horrible minigame now that I understand it.',
+            'Master difficulty? More like \"Cheating\" am I right?',
+            'Although I enjoyed Superstars more, I still enjoy playing Mario Party 9.'
+        ]
+        document.getElementById("div_quote").textContent = msgList[Math.floor(Math.random()*3)]
