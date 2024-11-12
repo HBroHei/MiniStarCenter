8b2c7504592d9dc2855a9b001f810ce51aecc9cb
hbrohei <hammerbrohei@gmail.com>
2024-11-12 22:06:22 +0800

    Updated for new space types & MGs

--- msg.js
+++ msg.js
@@ -1,6 +1,7 @@
 const msgList = [
             'Player Conveyor is a horrible minigame now that I understand it.',
             'Master difficulty? More like \"Cheating\" am I right?',
-            'Although I enjoyed Superstars more, I still enjoy playing Mario Party 9.'
+            'Although I enjoyed Superstars more, I still enjoy playing Mario Party 9.',
+            'All numbers in this website are either whole number or corrected to 3 significant figure.'
         ]
-        document.getElementById("div_quote").textContent = msgList[Math.floor(Math.random()*3)]
+        document.getElementById("div_quote").textContent = msgList[Math.floor(Math.random()*msgList.length)]
